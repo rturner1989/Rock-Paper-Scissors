ab461ec867a9a82e99ab1559232f1d29dabc8e9a
rturner1989 <robturner1000@hotmail.co.uk>
2022-02-15 19:04:42 +0000

    Adjusted animation for results

--- src/Components/Results Component/ResultsComponent.tsx
+++ src/Components/Results Component/ResultsComponent.tsx
@@ -20,8 +20,6 @@ const ResultsComponent: React.FC<props> = ({
     setCounter,
 }) => {
     const [result, setResult] = useState<string>("");
-    const [isBtnVisible, setIsBtnVisible] = useState(false);
-    const [isContVisible, setIsContVisible] = useState(false);
 
     function getResults(second: gameResult) {
         if (player.name === computer.name) return gameResult.DRAW;
@@ -50,40 +48,15 @@ const ResultsComponent: React.FC<props> = ({
         setCounter(updateCounter(results));
     }, [player, computer]);
 
-    useEffect(() => {
-        let timer = setTimeout(() => {
-            setIsBtnVisible(true);
-        }, 1000);
-        let timer2 = setTimeout(() => {
-            setIsContVisible(true);
-        }, 2500);
-        return () => {
-            clearTimeout(timer);
-            clearTimeout(timer2);
-        };
-    }, []);
-
     return (
-        <div
-            className={
-                isContVisible
-                    ? "results-container visible-container"
-                    : "results-container"
-            }
-        >
+        <div className="results-container">
             <div className="game-btn-container-result">
                 <h3>You picked</h3>
                 <button className={`result-btn ${player.name}-result`}>
                     <img src={player.image} alt={`${player.name} button`} />
                 </button>
             </div>
-            <div
-                className={
-                    isContVisible
-                        ? "reset-result-container reset-result-visible"
-                        : "reset-result-container"
-                }
-            >
+            <div className="reset-result-container">
                 <h2>{result}</h2>
                 <button className="reset-btn" onClick={reset}>
                     Play Again
@@ -96,11 +69,7 @@ const ResultsComponent: React.FC<props> = ({
                         : "Opponent Picked"}
                 </h3>
                 <button
-                    className={
-                        isBtnVisible
-                            ? `opponent-result-btn ${computer.name}-result visible`
-                            : `opponent-result-btn ${computer.name}-result`
-                    }
+                    className={`opponent-result-btn ${computer.name}-result`}
                 >
                     <img src={computer.image} alt={`${computer.name} button`} />
                 </button>
